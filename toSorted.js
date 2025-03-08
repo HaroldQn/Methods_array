32569c96620f05a3f6e3a014334219dc3d15e588
Harold Quispe <128565012+HaroldQn@users.noreply.github.com>
2025-03-07 23:49:29 -0500

    method arrays to...

---
+++ toSorted.js
@@ -0,0 +1,5 @@
+const numbers = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const order = numbers.toSorted((a, b) => a - b);
+console.log(numbers);
+console.log(order);
---
+++ toSpliced.js
@@ -0,0 +1,4 @@
+const tasks = ['Task 1', 'Task 2', 'Task 3', 'Task 4'];
+
+const res = tasks.toSpliced(1, 2, 'Task 5');
+console.log(res);
